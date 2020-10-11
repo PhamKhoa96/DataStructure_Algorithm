cd829986c43cf355a51846b7c3ac99ef614c24b0
= <=>
2020-10-12 00:28:36 +0700

    commit to repo

---
+++ OOP/33_2demensionArray.cpp
@@ -0,0 +1,36 @@
+// Mảng hai chiều trong C++
+/*	- khai báo
+	- khởi tạo
+	- truy xuất giá trị
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int a[10][15];
+	int b[100];
+	std::fill(b, b+100, 20);        //fill(from, to, value)
+
+	//std::fill(a[0], a[0] + 10 * 15, 1);
+
+	memset(a, 0, sizeof(a));
+
+	for (size_t i = 0; i < 100; i++)
+	{
+		cout << b[i] << " ";
+	}
+
+    cout << endl;
+
+	for (size_t i = 0; i < 10; i++)
+	{
+		for (size_t j = 0; j < 15; j++)
+		{
+			cout << a[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
